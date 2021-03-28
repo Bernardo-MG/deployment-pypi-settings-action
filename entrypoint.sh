ac0102a045b292172a53e7296262b4fe2f1b7220
Bernardo <programming@bernardomg.com>
2021-03-28 22:03:00 +0200

    Corrected docs

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,13 +9,6 @@
 # REMEMBER: For security reasons the data stored in the generated file should not be
 # shared. Never print it on the console or let it be accessed in any way.
 #
-# --- ENVIRONMENTAL VARIABLES ---
-#
-# The following environmental variables are required by the script:
-#
-# - DEPLOY_USER: string, user for the releases repo
-# - DEPLOY_PASSWORD: string, password for the releases repo
-#
 
 # Fails if any commands returns a non-zero value
 set -e
